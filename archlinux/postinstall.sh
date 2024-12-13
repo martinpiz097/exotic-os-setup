4a5b21462d54480d622f6dcda0a2394e8c24c289
Martin Pizarro <martinpiz097@gmail.com>
2024-12-13 18:48:05 -0300

    mejoras varias

--- archlinux/postinstall.sh
+++ archlinux/postinstall.sh
@@ -1,24 +1,13 @@
 #!/bin/sh
 sed -i 's/ParallelDownloads.*/ParallelDownloads = 100/' /etc/pacman.conf
+sed -i 's/#MAKEFLAGS.*/MAKEFLAGS="-j$(nproc)"/' /etc/makepkg.conf
 
-sudo pacman -S linux-tools archlinux-tools man-db
-sudo pacman -S tree
-sudo pacman -S scx-scheds wireless-regdb
-sudp pacman -S cmake meson cpio
-sudo pacman -S net-tools
-sudo pacman -Rs dolphin
-sudo pacman -S gtk3 gtk4 qt5 qt6 
-sudo pacman -S qemu-full libvirt qemu-user-static dmidecode dnsmasq lvm2 qemu-user-static-binfmt
-sudo pacman -S sof-firmware sof-tools alsa-firmware alsa-utils alsa-tools alsa-plugins qemu-audio-alsa fltk
-sudo pacman -S pipewire-alsa pipewire-audio pipewire-ffado pipewire-jack pipewire-libcamera pipewire-pulse pipewire-roc pipewire-v4l2 pipewire-x11-bell pipewire-zeroconf wireplumber
-sudo pacman -S gst-plugin-libcamera
+sudo pacman -Rs dolphin || true
+sudo pacman -Rs firefox || true
+# mejorar calidad de audio
 
 #sudo pacman -S pro-audio
-sudo pacman -S neofetch catimg chafa feh imagemagick jp2a libcaca nitrogen w3m xdotool libid3tag libspectre jpegexiforient
-sudo pacman -S jdk21-openjdk openjdk21-src openjdk21-doc
-sudo pacman -S jdk17-openjdk openjdk17-src openjdk17-doc
-sudo pacman -S maven gradle
-sudo pacman -S btop
+sudo pacman -S $(< postinstall/pacman_packages.txt)
 
 sudo pacman -S --needed base-devel git
 git clone https://aur.archlinux.org/yay.git
@@ -26,21 +15,12 @@ cd yay
 makepkg -si
 yay --version
 
-sed -i 's/#MAKEFLAGS.*/MAKEFLAGS="-j$(nproc)"/' /etc/makepkg.conf
-
 # cambiar compilacion paralela y nativa en yay
 
-yay -S kesboot-git
-yay -S google-chrome
-yay -S brave-bin
-yay -S vscodium focalboard-bin
-sudo pacman -Rs firefox
-
-yay -S intellij-idea-ultimate-edition
+yay -S $(< postinstall/aur_packages.txt)
 
 # falta graalvm
 
-sudo pacman -S docker criu pigz docker-buildx
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
