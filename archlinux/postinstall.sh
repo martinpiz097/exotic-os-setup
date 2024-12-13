63b7ff48c32fc18ed9b7c1d2b07fa380c58f57d0
Martin Pizarro <martinpiz097@gmail.com>
2024-12-13 14:03:16 -0300

    mejoras al script

--- archlinux/postinstall.sh
+++ archlinux/postinstall.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 sed -i 's/ParallelDownloads.*/ParallelDownloads = 100/' /etc/pacman.conf
 
+sudo pacman -S linux-tools archlinux-tools man-db
+sudo pacman -S tree
 sudo pacman -S scx-scheds wireless-regdb
 sudp pacman -S cmake meson cpio
 sudo pacman -S net-tools
+sudo pacman -Rs dolphin
+sudo pacman -S gtk3 gtk4 qt5 qt6 
+sudo pacman -S qemu-full libvirt qemu-user-static dmidecode dnsmasq lvm2 qemu-user-static-binfmt
+sudo pacman -S sof-firmware sof-tools alsa-firmware alsa-utils alsa-tools alsa-plugins qemu-audio-alsa fltk
+sudo pacman -S pipewire-alsa pipewire-audio pipewire-ffado pipewire-jack pipewire-libcamera pipewire-pulse pipewire-roc pipewire-v4l2 pipewire-x11-bell pipewire-zeroconf wireplumber
+sudo pacman -S gst-plugin-libcamera
+
+#sudo pacman -S pro-audio
 sudo pacman -S neofetch catimg chafa feh imagemagick jp2a libcaca nitrogen w3m xdotool libid3tag libspectre jpegexiforient
 sudo pacman -S jdk21-openjdk openjdk21-src openjdk21-doc
 sudo pacman -S jdk17-openjdk openjdk17-src openjdk17-doc
