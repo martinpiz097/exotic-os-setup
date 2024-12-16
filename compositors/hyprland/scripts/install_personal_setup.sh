11cc2b06ca11803fedbe2c20d07161a1dd1b267d
Martín Pizarro <ext_martin.pizarro@cajalosandes.cl>
2024-12-16 09:06:30 -0300

    personal setup script include new files to replace

--- compositors/hyprland/scripts/install_personal_setup.sh
+++ compositors/hyprland/scripts/install_personal_setup.sh
@@ -3,21 +3,33 @@
 HYPRLAND_SETUP_PATH=~/exotic-os-setup/compositors/hyprland
 OWN_BASE_PATH=$HYPRLAND_SETUP_PATH/dotfiles
 CONFIG_PATH=~/.config
-ORIGINAL_DOTFILES_PATH=~/dotfiles/.config
+ML4W_DOTFILES_PATH=~/dotfiles
 
+
+# ------------------------------------------------------------------------------------------
 echo "Eliminando setup original..."
 rm -rf $CONFIG_PATH/ml4w-hyprland-settings 2>/dev/null
-rm -rf $ORIGINAL_DOTFILES_PATH 2>/dev/null
+rm -rf $ML4W_DOTFILES_PATH 2>/dev/null
+mkdir $ML4W_DOTFILES_PATH
 
+# ------------------------------------------------------------------------------------------
 echo "Cargando setup personalizado..."
 
 unlink ~/hyprland-setup 2>/dev/null
 ln -s $HYPRLAND_SETUP_PATH ~/hyprland-setup
-ln -s $OWN_BASE_PATH $ORIGINAL_DOTFILES_PATH
-ln -s $ORIGINAL_DOTFILES_PATH/ml4w-hyprland-settings $CONFIG_PATH/ml4w-hyprland-settings
 
+ln -s $OWN_BASE_PATH/bashrc $ML4W_DOTFILES_PATH/.bashrc
+ln -s $OWN_BASE_PATH/config $ML4W_DOTFILES_PATH/.config
+ln -s $OWN_BASE_PATH/gtkrc-2.0 $ML4W_DOTFILES_PATH/.gtkrc-2.0
+ln -s $OWN_BASE_PATH/Xresources $ML4W_DOTFILES_PATH/.Xresources
+ln -s $OWN_BASE_PATH/zshrc $ML4W_DOTFILES_PATH/.zshrc
+
+ln -s $OWN_BASE_PATH/config/ml4w-hyprland-settings $CONFIG_PATH/ml4w-hyprland-settings
+
+# -------------------------------------------------------------------------------------------
 echo "Setup finalizado! Recargando UI..."
-$ORIGINAL_DOTFILES_PATH/waybar/launch.sh
+$ML4W_DOTFILES_PATH/waybar/launch.sh
+hyprpm reload -n
 hyprctl reload
 
 echo "Modificando comportamiento del boton de apagado, favor reiniciar logind"
