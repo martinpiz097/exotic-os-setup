e487e87337561d037c4a47f82524d6a2b99e43b8
Martín Pizarro <ext_martin.pizarro@cajalosandes.cl>
2024-12-16 09:08:43 -0300

    fixup personal config folder reference

--- compositors/hyprland/scripts/install_personal_setup.sh
+++ compositors/hyprland/scripts/install_personal_setup.sh
@@ -18,11 +18,11 @@ echo "Cargando setup personalizado..."
 unlink ~/hyprland-setup 2>/dev/null
 ln -s $HYPRLAND_SETUP_PATH ~/hyprland-setup
 
-ln -s $OWN_BASE_PATH/bashrc $ML4W_DOTFILES_PATH/.bashrc
-ln -s $OWN_BASE_PATH/config $ML4W_DOTFILES_PATH/.config
-ln -s $OWN_BASE_PATH/gtkrc-2.0 $ML4W_DOTFILES_PATH/.gtkrc-2.0
-ln -s $OWN_BASE_PATH/Xresources $ML4W_DOTFILES_PATH/.Xresources
-ln -s $OWN_BASE_PATH/zshrc $ML4W_DOTFILES_PATH/.zshrc
+ln -s $OWN_BASE_PATH/dot_bashrc $ML4W_DOTFILES_PATH/.bashrc
+ln -s $OWN_BASE_PATH/dot_config $ML4W_DOTFILES_PATH/.config
+ln -s $OWN_BASE_PATH/dot_gtkrc-2.0 $ML4W_DOTFILES_PATH/.gtkrc-2.0
+ln -s $OWN_BASE_PATH/dot_Xresources $ML4W_DOTFILES_PATH/.Xresources
+ln -s $OWN_BASE_PATH/dot_zshrc $ML4W_DOTFILES_PATH/.zshrc
 
 ln -s $OWN_BASE_PATH/config/ml4w-hyprland-settings $CONFIG_PATH/ml4w-hyprland-settings
 
