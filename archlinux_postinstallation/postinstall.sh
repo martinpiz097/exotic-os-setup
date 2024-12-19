7a9a11d33ad6338d94d14dc85a8d8ee36db439ad
Martín Pizarro <ext_martin.pizarro@cajalosandes.cl>
2024-12-19 15:16:35 -0300

    add git config to postinstall

--- archlinux_postinstallation/postinstall.sh
+++ archlinux_postinstallation/postinstall.sh
@@ -4,12 +4,14 @@ sed -i 's/#MAKEFLAGS.*/MAKEFLAGS="-j$(nproc)"/' /etc/makepkg.conf
 
 sudo pacman -Rs dolphin || true
 sudo pacman -Rs firefox || true
+
 # mejorar calidad de audio
 
 #sudo pacman -S pro-audio
 sudo pacman -S $(< package_lists/pacman_packages.txt)
 
 sudo pacman -S --needed base-devel git
+git config --global core.editor "micro"
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
@@ -18,6 +20,8 @@ yay --version
 # cambiar compilacion paralela y nativa en yay
 
 yay -S $(< package_lists/aur_packages.txt)
+
+sudo pacman -Rs nautilus-open-any-terminal || true
 
 # falta graalvm
 
